c39539b6c4ebbe346900bcd75c4859855457a38d
patrickmilnes <pjamilnes@gmail.com>
2021-06-26 22:10:58 +0100

    feat: Added empty list check in print_list function.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 main.o: main.c linkedlist.o
-	gcc main.c build/linkedlist.o -o build/prog.out
+	gcc main.c build/linkedlist.o -o build/prog
 
 linkedlist.o: linkedlist.c
 	gcc linkedlist.c -c -o build/linkedlist.o
--- linkedlist.c
+++ linkedlist.c
@@ -8,22 +8,21 @@
  * 
  * @return LinkedList* 
  */
-LinkedList* init_linkedlist()
+LinkedList *init_linkedlist()
 {
 
-	// Attempt the allocation of the list
-	LinkedList* list = (LinkedList*) malloc(sizeof(LinkedList));
-	if(list == NULL)
-		return NULL;
+    // Attempt the allocation of the list
+    LinkedList *list = (LinkedList *)malloc(sizeof(LinkedList));
+    if (list == NULL)
+        return NULL;
 
-	// Assign 
-	list->head = NULL;
-	list->tail = NULL;
-	list->length = 0;
+    // Assign
+    list->head = NULL;
+    list->tail = NULL;
+    list->length = 0;
 
-	// Return pointer
-	return list;
-
+    // Return pointer
+    return list;
 }
 
 /**
@@ -31,13 +30,20 @@ LinkedList* init_linkedlist()
  * 
  * @param list 
  */
-void print_list(LinkedList* list) 
+void print_list(LinkedList *list)
 {
     printf("Printing list:\n");
-    Node* temp = list->head;
+    Node *temp = list->head;
 
-    while(temp != NULL) {
-        my_payload_t* payload = (my_payload_t*) temp->payload;
+    if (list->length == 0)
+    {
+        printf("List is empty!\n");
+        return;
+    }
+
+    while (temp != NULL)
+    {
+        my_payload_t *payload = (my_payload_t *)temp->payload;
         printf("%i\t%s\n", payload->age, payload->name);
         temp = temp->next;
     }
@@ -94,20 +100,20 @@ int push_node(LinkedList *list, void *payload, size_t n)
  * @param destination 
  * @return int 
  */
-int pop_node(LinkedList* list)
+int pop_node(LinkedList *list)
 {
-    if(list == NULL || list->length == 0)
+    if (list == NULL || list->length == 0)
         return 1;
 
     // Unlink head from list.
-    Node* to_free = list->head;
+    Node *to_free = list->head;
     list->head = to_free->next;
     list->length--;
 
     free(to_free);
     free(to_free->payload);
 
-    if(list->length == 0)
+    if (list->length == 0)
         list->tail == NULL;
 
     return 0;
